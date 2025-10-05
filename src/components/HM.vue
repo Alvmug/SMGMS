340bbd95c49561b7d8cbdbe6ce65537f35c6e200
Alves <alvmug09@gmail.com>
2025-10-05 13:10:21 +0200

    3rd Checkpoint

--- src/components/HM.vue
+++ src/components/HM.vue
@@ -68,7 +68,25 @@
     <div class="box1">
       <h1><FontAwesomeIcon :icon="faClock" class="clock" /> Teaching Schedule</h1>
       <div class="null"></div>
-      <div class="Level"><h1>L3MMP</h1></div>
+      <div class="Level">
+        <div class="time">
+          <h1>8:00 - 8:40</h1>
+        </div>
+        <h1>L3MMP</h1>
+        <div class="GD">
+          <h1>Graphics Design</h1>
+        </div>
+      </div>
+      <div class="Level4">
+        <div class="null"></div>
+        <div class="time2">
+          <h1>8:00 - 8:40</h1>
+        </div>
+        <h1>L4SOD</h1>
+        <div class="DS">
+          <h1>Data Structure</h1>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -78,6 +96,25 @@ import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import { faMicrochip } from '@fortawesome/free-solid-svg-icons'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { onMounted } from 'vue'
+import { onUnmounted } from 'vue'
+import _ from 'lodash'
+
+const handleScroll = _.debounce(() => {
+  const navbar = document.querySelector('.nav')
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}, 10)
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 <style>
 .body {
@@ -98,6 +135,10 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   height: fit-content;
   width: 100vw;
   top: 18%;
+}
+.nav.scrolled {
+  box-shadow: 0px 6px 4px rgb(6, 6, 6);
+  transition: all 0.1ms ease-out;
 }
 .routes {
   text-decoration: none;
@@ -159,18 +200,18 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   color: black;
 }
 .students:nth-child(1) {
-  box-shadow: 1px 1px 49px rgb(156, 155, 155);
+  box-shadow: 4px 5px 30px rgb(190, 190, 190);
 }
 .students:nth-child(2) {
-  box-shadow: 1px 1px 49px rgb(156, 155, 155);
+  box-shadow: 4px 5px 30px rgb(190, 190, 190);
   left: 33%;
 }
 .students:nth-child(3) {
-  box-shadow: 0px 0px 49px rgb(156, 155, 155);
+  box-shadow: 4px 5px 30px rgb(190, 190, 190);
   left: 59%;
 }
 .students:nth-child(4) {
-  box-shadow: 1px 1px 49px rgb(156, 155, 155);
+  box-shadow: 4px 5px 30px rgb(190, 190, 190);
   left: 59%;
 }
 .students h1:nth-child(3) {
@@ -184,7 +225,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   display: flex;
   position: absolute;
   width: 35rem;
-  height: 19rem;
+  height: 24rem;
   background: rgb(246, 241, 241);
   top: 90%;
   left: 1%;
@@ -197,7 +238,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   top: 10%;
   left: 2%;
   font-weight: 450;
-  opacity: 75%;
+
   font-size: 25px;
   color: black;
 }
@@ -214,10 +255,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 }
 .clock {
   position: relative;
-  left: 150%;
-  opacity: 100%;
+  left: 190%;
   color: black;
-  font-size: 40px;
+  font-size: 30px;
 }
 .null {
   width: 0.4em;
@@ -225,13 +265,57 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   border-radius: 5px;
   top: 30%;
   left: 10%;
-  height: 9em;
+  height: 7em;
   background: rgb(21, 21, 21);
+}
+.null:nth-child(1) {
+  left: -29%;
+  background-color: rgb(60, 60, 60);
 }
 .Level {
   color: black;
   position: relative;
   top: 30%;
   left: 15%;
+}
+.GD {
+  position: absolute;
+  width: 200px;
+  color: rgb(60, 59, 59);
+  font-size: 10px;
+  top: 22%;
+}
+.Level4 {
+  color: black;
+  position: absolute;
+  top: 63%;
+  left: 15%;
+}
+.DS {
+  color: rgb(60, 59, 59);
+  font-size: 10px;
+  top: 119%;
+  width: 200px;
+  position: absolute;
+}
+.time {
+  position: absolute;
+  font-size: 10px;
+  left: 350%;
+  width: 100px;
+  opacity: 50%;
+  padding: 3px;
+  background: rgba(41, 171, 130, 0.386);
+  border-radius: 12px;
+}
+.time2 {
+  position: absolute;
+  font-size: 10px;
+  left: 390%;
+  width: 100px;
+  opacity: 80%;
+  padding: 3px;
+  background: rgb(22, 107, 255);
+  border-radius: 12px;
 }
 </style>
